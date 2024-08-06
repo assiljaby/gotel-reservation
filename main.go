75034d128866959c8fdf90d59d0ff5b15f1e13d9
AltF4irl <altf4irl@gmail.com>
2024-08-07 02:28:25 +0300

    Added: simple foo page

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/gofiber/fiber/v3"
+
 func main() {
-	
+	app := fiber.New()
+	app.Get("/foo", handleFoo)
+	app.Listen(":3000")
+}
+
+func handleFoo(c fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "Bar!"})
 }
