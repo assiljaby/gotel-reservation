a90c0209c0648c902ee8efc50554b53bde77bf7d
AltF4irl <altf4irl@gmail.com>
2024-08-08 23:32:53 +0300

    Added: mongodb driver

--- main.go
+++ main.go
@@ -1,13 +1,56 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/assiljaby/gotel-reservation/api"
+	"github.com/assiljaby/gotel-reservation/types"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dbName = "gotel-reservation"
+const userCollection = "users"
+
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongodbURI := os.Getenv("MONGODB_URI")
+	// mongodbAtlasURI := os.Getenv("MONGODB_ATLAS_URI")
+	if mongodbURI == "" {
+		log.Fatal("Database URI was not set correctly.")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongodbURI))
+	if err != nil {
+		log.Fatal("Failed to connect to database.")
+	}
+
+	// ctx := context.Background()
+	coll := client.Database(dbName).Collection(userCollection)
+
+	scrubloard := types.User{
+		FirstName: "Scrub",
+		LastName: "Lord",
+	}
+
+	res, err := coll.InsertOne(context.TODO(), scrubloard)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+
 	listenPort := flag.String("listenPort", ":3000", "The server is listening to this port")
 	flag.Parse()
 
