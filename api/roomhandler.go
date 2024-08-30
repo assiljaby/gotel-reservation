ffbf361b9c3eeee7fda910fd455dd8092704f6a0
AltF4irl <altf4irl@gmail.com>
2024-08-30 21:33:33 +0300

    Refractor: room and hotel handler

---
+++ api/roomhandler.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"github.com/assiljaby/gotel-reservation/db"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type RoomHandler struct {
+	store *db.Store
+}
+
+func NewRoomHandler(store *db.Store) *RoomHandler {
+	return &RoomHandler{
+		store: store,
+	}
+}
+
+func (h *RoomHandler) HandleGetRooms(c *fiber.Ctx) error {
+	rooms, err := h.store.Room.GetRooms(c.Context(), bson.M{})
+	if err != nil {
+		return err
+	}
+	return c.JSON(rooms)
+}
